585b451bb73981275541587231afa7f184f48be6
thrsnknwldgthtsntpwr <thrsnknwldgthtsntpwr@yandex.ru>
2025-04-07 23:14:09 +0000

    java-fixed

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,7 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
-	public String saySome(){
+	public String sayHunter(){
 		return "Forgive Hunter Biden";
 	}
 }
